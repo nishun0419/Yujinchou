151d394e099859bf997899aacc0ea9e850cd499e
nishun0419 <ninosekunlight@gmail.com>
2018-10-06 18:59:23 +0900

    Initial Commit

---
+++ Yujincho/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yujincho
+//
+//  Created by shin on 2018/10/06.
+//  Copyright © 2018年 nishun0419. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
